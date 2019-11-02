107234d4753536d710533d2c12dc0e720533f56f
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-11-02 10:52:13 -0400

    Correcting repository name.

--- member/lookupService.js
+++ member/lookupService.js
@@ -1,7 +1,7 @@
-var accountRepository = require('./accountRepository')
+var memberRepository = require('./memberRepository')
 var configurationService = require("../configuration/configurationService")
 
 exports.findAccount = async function(accountNumber, db) {
     return await configurationService.findBloomerangBaseApiUrl(db)
-      .then(bloomerangBaseApiUrl => accountRepository.findAccount(accountNumber, bloomerangBaseApiUrl));
+      .then(bloomerangBaseApiUrl => memberRepository.findAccount(accountNumber, bloomerangBaseApiUrl));
   }
--- member/verificationService.js
+++ member/verificationService.js
@@ -1,9 +1,9 @@
-var accountRepository = require('./accountRepository')
+var memberRepository = require('./memberRepository')
 var configurationService = require("../configuration/configurationService")
 
 exports.verifyAccount = async function(accountNumber, db) {
     return await configurationService.findBloomerangBaseApiUrl(db)
-      .then(bloomerangBaseApiUrl => accountRepository.findAccount(accountNumber, bloomerangBaseApiUrl)
+      .then(bloomerangBaseApiUrl => memberRepository.findAccount(accountNumber, bloomerangBaseApiUrl)
       .then(accounts => {
         return accounts.Total > 0;
       }));
