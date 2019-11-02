3373427f0b8d5f2b3c38183bd01bae5f75bb5ae2
Lisa Lemons <lisalemons@Lisas-MacBook-Pro.local>
2019-11-02 17:12:43 -0400

    submit number commented out for now

--- src/app/member-checkin/calculator/member-checkin-calculator.component.ts
+++ src/app/member-checkin/calculator/member-checkin-calculator.component.ts
@@ -3,6 +3,7 @@ import { MemberService } from '@services/member.service';
 import { Observable } from 'rxjs';
 import { Member } from '@models/member';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-checkin-calculator',
@@ -13,7 +14,10 @@ export class MemberCheckinCalculatorComponent implements OnInit {
 
   userId = '';
   numbers = [];
-  
+  isVerified$: Observable<boolean> =  this.memberService.verifyMember(this.userId).pipe(
+    isVerified => isVerified
+  );
+
   constructor(public memberService: MemberService, public router: Router) {
   }
 
@@ -36,22 +40,28 @@ export class MemberCheckinCalculatorComponent implements OnInit {
     }
   }
 
-  public submitNumber(userId: string) { 
-    /** 
+  public submitNumber() {
+    /**
      * failing isn't working
-     * we need to set it up to return to home if member doesn't exist */ 
+     * we need to set it up to return to home if member doesn't exist */
 
-    if (this.userId.length === 4) {
-      this.memberService.verifyMember(this.userId)
-        .pipe(isVerified => {
-          console.log(isVerified);
-          if (isVerified) {
-            return this.router.navigateByUrl('member-checkin/pictures');
-          } else {
-            return this.router.navigateByUrl('/');
-          }
-        });
-    }
-    
+    // if (this.userId.length === 4) {
+    //   console.log('why')
+    //   this.memberService.verifyMember(this.userId)
+    //     .pipe(
+    //       map(isVerified => {
+    //         console.log({isVerified})
+    //         if (isVerified) {
+    //           console.log('why is verified')
+    //           this.router.navigateByUrl('member-checkin/pictures');
+    //         } else {
+    //           console.log('why is not verified')
+    //           this.router.navigateByUrl('/');
+    //         }
+    //         return;
+    //       })
+    //     );
+    // }
+
   }
 }
