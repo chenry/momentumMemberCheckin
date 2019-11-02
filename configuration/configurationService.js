ce5de362d91119cb9240aaf32802593c444c937a
steve poling <sdpoling@gmail.com>
2019-11-02 13:40:26 -0400

    implement survey urls

--- configuration/configurationService.js
+++ configuration/configurationService.js
@@ -5,6 +5,10 @@ exports.findBloomerangBaseApiUrl = async function(db) {
   return await findConfigurationValueByKey(db, "bloomerangBaseApiUrl");
 }
 
+exports.findAllUrls = async function(db) {
+  return await configurationRepository.findConfiguration(db);
+}
+
 async function findConfigurationValueByKey(db, key) {
   let config = await configurationRepository.findConfiguration(db);
   return config[key];
