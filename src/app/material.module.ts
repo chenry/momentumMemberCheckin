176a5963b6c1c6330988764954b0548a2030a7b8
TomasP <perezgantomas@gmail.com>
2019-11-02 00:01:37 -0400

    member-heckin-pictures

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,18 @@
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { NgModule } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   imports: [
     MatToolbarModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatGridListModule
   ],
   exports: [
     MatToolbarModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatGridListModule
   ]
 })
 
