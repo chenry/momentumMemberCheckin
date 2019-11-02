0b3af58c0c92e916966a28e627386a05a9d022d9
TomasP <perezgantomas@gmail.com>
2019-11-02 19:28:20 -0400

    Login Page routed

---
+++ src/app/member-checkin/admin-login/admin-login-component.spect.ts
@@ -0,0 +1,9 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MemberCheckinAdminLoginComponent } from './member-checkin.component';
+
+
+describe('MemberCheckinAdminLoginComponent', () => {
+  let component: MemberCheckinAdminLoginComponent;
+  let fixture: ComponentFixture<MemberCheckinAdminLoginComponent>;
+})
