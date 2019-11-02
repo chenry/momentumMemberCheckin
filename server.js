d65e07fae4d4eca5f5d0b9c40ab439a07935050a
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-11-01 23:11:07 -0400

    Adding documentation for required environment variables.

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// Required environment variables:
+// MONGODB_URI - This should contain the full uri to the MongoDB
+// BLOOMERANG_KEY - This should contain the private key of the bloomerang api.
+
 var express = require("express");
 const fetch = require('node-fetch');
 var bodyParser = require("body-parser");
