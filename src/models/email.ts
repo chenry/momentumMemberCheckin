c162f0a03603da55dfe813af85dd211e05bc116a
Lisa Lemons <lisalemons@Lisas-MacBook-Pro.local>
2019-11-02 10:29:28 -0400

    Added new models

---
+++ src/models/email.ts
@@ -0,0 +1,9 @@
+export class Email {
+  public AccountId: number;
+  public IsAccountIdApplied: boolean;
+  public TypeName: string;
+  public Value: string;
+  public IsPrimary: boolean;
+  public IsBad: boolean;
+  public Id: number;
+}
