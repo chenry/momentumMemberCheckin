9b3fba355fac54738e7689bf3f0e61532923469e
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2019-11-03 12:06:46 -0500

    Fixing issue with 6 month task due calculation.

--- timeline/timelineParser.js
+++ timeline/timelineParser.js
@@ -27,17 +27,18 @@ exports.findInteractions = function(timeline) {
 
 exports.createOpenTasksResponse = function(openTasks) {
   return {
-    sixMonthTask: contains6MonthTask(openTasks)
+    sixMonthTask: is6MonthTaskDue(openTasks)
   }
 }
 
-function contains6MonthTask(openTasks) {
+function is6MonthTaskDue(openTasks) {
+  let now = new Date();
   for (let currTask of openTasks) {
+    const dueDate = new Date(currTask.DueDate);
 
-    if (constants.SIX_MONTH_TASK_SUBJECT == currTask.Subject) {
+    if (constants.SIX_MONTH_TASK_SUBJECT == currTask.Subject && now > dueDate) {
       return true;
     }
   }
   return false;
 }
-
