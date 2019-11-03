5e411b3334001683604437e7bb3f16eebda06456
Lisa Lemons <lisalemons@Lisas-MacBook-Pro.local>
2019-11-02 20:17:03 -0400

    set up a form group for example purposes

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
-
-
-
-
 })
 
 export class AdminLoginComponent implements OnInit {
+  options: FormGroup;
 
-  constructor() {
+  constructor(formBuilder: FormBuilder) {
+    this.options = formBuilder.group({
+      secretCode: ['', Validators.minLength(1)]
+    });
   }
 
   ngOnInit() {
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   imports: [
@@ -12,7 +13,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
     MatButtonModule,
     MatGridListModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatFormFieldModule
   ],
   exports: [
     MatToolbarModule,
@@ -20,7 +22,8 @@ import { MatGridListModule } from '@angular/material/grid-list';
     MatButtonModule,
     MatGridListModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatFormFieldModule
   ]
 })
 
