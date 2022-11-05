f6879da98db1d4b9605d9de579aa528f890d505c
Carlus Henry <carlushenry@sagetech-llc.com>
2022-11-05 01:34:14 -0400

    fixing the issue with connecting to mongo locally

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 var express = require("express");
 const fetch = require('node-fetch');
 var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb");
 const knex = require('./lib/db')
 
 // ====================
@@ -25,11 +25,6 @@ var adminResetRegistrationService = require('./admin/resetRegistrationService')
 var surveyUrlGeneratorService = require('./member/surveyUrlGeneratorService')
 // ====================
 
-
-
-var ObjectID = mongodb.ObjectID;
-
-
 var CONTACTS_COLLECTION = "contacts";
 var IMAGES_COLLECTION = "images";
 
@@ -41,10 +36,9 @@ var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
+const uri = process.env.DB_URI || 'mongodb://root:rootpassword@localhost:27017/momentumMongoDB?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256'
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.DB_URI || "mongodb://root:rootpassword@localhost:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256", { useUnifiedTopology: true }, function (err, client) {
+MongoClient.connect(uri, { useUnifiedTopology: true }, function (err, client) {
   if (err) {
     console.log(err);
     process.exit(1);
